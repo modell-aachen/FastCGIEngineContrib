3be67c7c85cfc78c247afbe6c0a9ede316c8b74d
Stephan Osthold <osthold@modell-aachen.de>
2017-11-07 12:17:20 +0100

    ID:3802 Remove SVN version string
    Ticket: none
    Release-Note: none

--- lib/Foswiki/Contrib/FastCGIEngineContrib.pm
+++ lib/Foswiki/Contrib/FastCGIEngineContrib.pm
@@ -17,7 +17,7 @@ use strict;
 
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
-$VERSION          = '$Rev$';
+$VERSION          = '0.9';
 $RELEASE = "0.9";
 $SHORTDESCRIPTION = 'Permits Foswiki to be executed with FastCGI';
 
